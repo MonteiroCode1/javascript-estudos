d0747ce3c5493f43720601fecf10fcc0460ead6a
Guilherme Monteiro <guilhermemonteiroduarte851@gmail.com>
2026-01-23 14:39:59 -0300

    conhecenod duas funçoes, Sort, reverse.

---
+++ cap06/HTML/JavaScript/SortReverse.js
@@ -0,0 +1,17 @@
+const prompt = require("prompt-sync")();
+
+// usando a funçao sort() ordena em ordem Alfabetica
+const nomes = ["Derique", "Bazim", "Ciene", "Antonio", "Erique"];
+console.log(nomes.join(", "));
+console.log(nomes.sort().join(", "));
+
+// usando a funçao reverse() como nome diz faz o reverso de trais pra frente
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(numeros.reverse().join(", "));
+const num = numeros.reverse();
+console.log(num.sort().join(", "));
+console.log(numeros.sort())
+
+const valores = [50, 100, 2];
+console.log(valores.sort);
