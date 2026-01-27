46894f9fafea2a66dc3fe0dfef4942fbac835faa
Guilherme Monteiro <guilhermemonteiroduarte851@gmail.com>
2026-01-27 11:41:11 -0300

    metodo de Listar em HTML

--- cap06/HTML/JavaScript/ex6_4.js
+++ cap06/HTML/JavaScript/ex6_4.js
@@ -3,18 +3,27 @@ const resp = document.querySelector("pre");
 
 const vetor = [];
 
-const acumulador = ``;
-
 frm.addEventListener("submit", (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const nome = frm.inNome.value;
     const idade = Number(frm.inIdade.value);
+    vetor.push({nome, idade});
+    frm.reset(); // reseta os campos
+    frm.inNome.focus(); // cursor do mause vai para o campo
+    frm.inListar.dispatchEvent(new Event("click")); // dispara click em inLista
+})
 
-    acumulador += `Nome: ${nome}, Idade: ${idade}\n`;
-    vetor.push([nome, idade]);
-});
 
-
-frm.inLista.addEventListener("click", () => {
+frm.inListar.addEventListener("click", () => {
+    if (vetor.length == 0) {
+        alert("Nosso vetor tem zero Crianças");
+        return;
+    }
+    let acumulador = ``;
+    for (const criança of vetor) {
+        const {nome, idade} = criança;
+        acumulador += `Nome: ${nome}, Idade: ${idade}\n`;
+    }
     resp.innerText = acumulador;
-});
+
+})
