20e7d4ffa88c25fcb38128deb4b87dbc26f8886f
Guilherme Monteiro <guilhermemonteiroduarte851@gmail.com>
2026-01-28 17:36:39 -0300

    update inListar in Java Script

--- cap06/HTML/JavaScript/ex6_4.js
+++ cap06/HTML/JavaScript/ex6_4.js
@@ -27,3 +27,35 @@ frm.inListar.addEventListener("click", () => {
     resp.innerText = acumulador;
 
 })
+
+frm.inResumir.addEventListener("click", () => {
+    if (vetor.length == 0) {
+        alert("Zero Criaças no Vetor");
+        return;
+    }
+
+    const copia = [...vetor]; // cria copia do vetor criaça
+    copia.sort((a, b) => a.idade - b.idade); // ordena pela idade
+    let resumo = "";
+    let aux = copia[0].idade; // pra auxiliar
+    let nomes = []; // para inserir nomes de cada idade
+    for (const criaças of copia) { 
+        const {nome, idade} = criaças;
+        if (idade == aux) { // se é da mesma idade auxiliar
+            nomes.push(nome); // adiciona no nosso vetor nomes
+        }
+        else {
+            resumo += aux + " ano(s): " + nomes.length + " crianças - ";
+            resumo += ((nomes.length / copia.length) * 100).toFixed(2) + "%\n";
+            resumo += "(" + nomes.join(", ") + ") \n\n";
+            aux = idade;
+            nomes = [];
+            nomes.push(nome)
+        }
+    }
+    //adiciona a ultema criaçao
+    resumo += aux + " ano(s): " + nomes.length + "criança(s) - ";
+    resumo += ((nomes.length / copia.length) * 100).toFixed(2) + "%\n";
+    resumo += "(" + nomes.join(", ") + ") \n\n";
+    resp.innerText = resumo;
+})
