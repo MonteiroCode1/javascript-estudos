7dc673928d9afbecc4f4bf7d0fc6ddf03ee6462d
MonteiroCode1 <guilhermemonteiroduarte851@gmail.com>
2026-02-10 23:57:29 -0300

    Exercicio Numeros em Ordem V1, n ta fucionando

---
+++ cap06/JavaScript/numeros_ordem.js
@@ -0,0 +1,28 @@
+const frm = document.querySelector("form");
+const resp1 = document.querySelector("#resp1");
+const resp2 = document.querySelector("#resp2");
+
+// Vetor Global
+const numeros = [];
+
+// verificar um evento no botao submit
+frm.addEventListener("submit", (e) => {
+    e.preventDefaut();
+    const valor = Number(frm.inNumero.value);
+    if (numeros.length == 0) {
+        numeros.push(valor);
+    } else {
+        let r = false;
+        for (const e of numeros) {
+            if (valor == e) {
+                r = true;
+            }
+        }
+        if (r) {
+            alert(`O numero ${valor} ja existe no vetor`);
+        } else {
+            numeros.push(valor);
+            resp1.innerText = `Numeros: ${numeros.join(", ")}`;
+        }    
+    }
+})
