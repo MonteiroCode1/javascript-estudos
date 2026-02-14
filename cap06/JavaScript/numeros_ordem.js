82944445cb980fa2b733d13f9b32a9396fe8bf94
Guilherme Monteiro <guilhermemonteiroduarte851@gmail.com>
2026-02-14 09:57:17 -0300

    Conclusão do Exercicio sobre numeros em Ordem

--- cap06/JavaScript/numeros_ordem.js
+++ cap06/JavaScript/numeros_ordem.js
@@ -11,19 +11,41 @@ frm.addEventListener("submit", (x) => {
     const valor = Number(frm.inNumero.value);
     if (numeros.length == 0) {
         numeros.push(valor);
+        frm.inNumero.value = "";
+        frm.inNumero.focus();
         resp1.innerText= `Numeros: ${numeros.join(", ")}`;
     } else {
         let r = false;
         for (const e of numeros) {
             if (valor == e) {
                 r = true;
+                break;
             }
         }
         if (r) {
             alert(`O numero ${valor} ja existe no vetor`);
         } else {
             numeros.push(valor);
+            frm.inNumero.value = "";
+            frm.inNumero.focus();
             resp1.innerText = `Numeros: ${numeros.join(", ")}`;
         }    
+    }
+})
+
+// Verificar Ordem
+
+frm.inOrdem.addEventListener("click", (e) => {
+    let r = true;
+    for (let i = 0; i < numeros.length - 1; i++) {
+        if (numeros[i] > i+1) {
+            r = false;
+            break;
+        }
+    }
+    if (r) {
+        resp2.innerText = "Os Numeros estão em Ordem Crescente!";
+    } else {
+        resp2.innerText = "Os Numeros Não Estão em Ordem Crescente";
     }
 })
