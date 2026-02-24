12187e3344b6c339f28227b02451b7c2c2aa8417
Guilherme Monteiro <guilhermemonteiroduarte851@gmail.com>
2026-02-24 16:44:07 -0300

    sistema pedidos versao 1

---
+++ cap07/exercicios/Sistema-de-pedidos/script.js
@@ -0,0 +1,36 @@
+const frm = document.querySelector("form");
+const resp = document.querySelector("pre");
+
+const pedidos = [
+  {
+    cliente: "Ana",
+    itens: [
+      { produto: "Teclado", preco: 120, qtd: 1 },
+      { produto: "Mouse", preco: 60, qtd: 2 }
+    ]
+  },
+  {
+    cliente: "Carlos",
+    itens: [
+      { produto: "Monitor", preco: 900, qtd: 1 }
+    ]
+  },
+  {
+    cliente: "Beatriz",
+    itens: [
+      { produto: "Webcam", preco: 180, qtd: 1 },
+      { produto: "Headset", preco: 250, qtd: 1 }
+    ]
+  }
+];
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let = soma = 0;
+    let exibir = ``;
+    const total = pedidos.length - 1;
+    do {
+
+        
+    } while(total > 0)
+})
