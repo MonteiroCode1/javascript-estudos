649dfc44d3c360b1ec2653b5151a05e807f19697
Guilherme Monteiro <guilhermemonteiroduarte851@gmail.com>
2026-02-27 14:00:18 -0300

    Conclução do Sistema de Pedidos

--- cap07/exercicios/Sistema-de-pedidos/script.js
+++ cap07/exercicios/Sistema-de-pedidos/script.js
@@ -25,16 +25,18 @@ const pedidos = [
 // Mostra o Total de Cada Pedido.
 let x = 0;
 let TotalDeCadaUm = [];
+let QuantidadeDeCadaProduto = [];
 do {
   const nome = pedidos[x].cliente;
   const produtos = pedidos[x].itens;
   let soma = 0
   for(const elemento of produtos) {
-    const {preco, qtd} = elemento;
+    const {produto, preco, qtd} = elemento;
+    QuantidadeDeCadaProduto.push({produto, qtd});
     soma += preco * qtd;
   }
   TotalDeCadaUm.push({nome, soma});
-  console.log(`${nome}: R$${soma}`);
+  console.log(`${nome}: R$${soma.toFixed(2)}`);
   x++;
 } while(x < pedidos.length)
 
@@ -44,3 +46,20 @@ do {
 const maior = TotalDeCadaUm.reduce((acumulador, elemento) => Math.max(acumulador, elemento.soma), 0);
 const GastouMais = TotalDeCadaUm.filter(elemento => elemento.soma == maior);
 console.log(`\nCliente que mais Gastou: ${GastouMais[0].nome}.`)
+
+
+const MaisVendido = QuantidadeDeCadaProduto.reduce((acumulador, elemento) => Math.max(acumulador, elemento.qtd), 0);
+
+const ProdMaisVendido = QuantidadeDeCadaProduto.filter(elemento => elemento.qtd == MaisVendido);
+
+for (let i = 0; i < ProdMaisVendido.length; i++) {
+  console.log(`Produto Mais Vendido: ${ProdMaisVendido[i].produto} (${ProdMaisVendido[i].qtd} unidades)`);
+}
+
+
+let total = 0;
+for (const objeto of TotalDeCadaUm) {
+  total += objeto.soma;
+}
+
+console.log(`Total de Vendas da Loja: R$ ${total.toFixed(2)}`);
