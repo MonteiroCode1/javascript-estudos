03f85c5c4ed8bd95291adee89a6f61f41ec72070
monteirocode1 <guilhermemonteiroduarte851@gmail.com>
2026-03-09 18:50:54 -0300

    correção

--- cap07/javascript/update-padStart.js
+++ cap07/javascript/update-padStart.js
@@ -1,7 +1,6 @@
 const hoje = new Date();
 
 // obter o dia, o mẽs e o ano;
-const dia = hoje.getDate().toString(), mes = hoje.getMonth() + 1, ano = hoje.getFullYear().toString();
-const mes2 = mes.toString();
-console.log(`Data: ${dia.padStart(2, "0")}/${mes2.padStart(2, "0")}/${ano}`);// o metodo padStart so Fuciona se seus dados forem do tipo string.
+const dia = hoje.getDate().toString(), mes = (hoje.getMonth() + 1).toString(), ano = hoje.getFullYear().toString();
+console.log(`Data: ${dia.padStart(2, "0")}/${mes.padStart(2, "0")}/${ano}`);// o metodo padStart so Fuciona se seus dados forem do tipo string.
 
