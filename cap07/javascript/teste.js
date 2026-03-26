c1194287a54995775939c2f8cef1aab522da5a4a
Guilherme Monteiro <guilhermemonteiroduarte851@gmail.com>
2026-03-26 14:30:20 -0300

    Update teste.js

--- cap07/javascript/teste.js
+++ cap07/javascript/teste.js
@@ -2,7 +2,7 @@ const prompt = require("prompt-sync")();
 
 const frase = prompt("Frase: ");
 
-const frase1 = [... frase.replace(/ /g, "")].join("");
+const frase1 = [...frase.replace(/ /g, "")].join("");
 const frase2 = [...frase1].reverse().join("");
 console.log(frase1);
 console.log(frase2);
