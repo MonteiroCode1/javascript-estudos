f6bb625f75d9a45ff8ec4db67fe1e91e721d8d79
Guilherme Monteiro <guilhermemonteiroduarte851@gmail.com>
2026-03-26 15:31:24 -0300

    exercicios versão 0.3

--- cap07/javascript/script02.js
+++ cap07/javascript/script02.js
@@ -4,6 +4,15 @@ const resp = document.querySelector("h3");
 frm.addEventListener("submit", (e) => {
     e.preventDefault();
     const frase = frm.inFrase.value;
-    const RemoverEspaços = frase.replace(/ /g, "");
+    const frase1 = frase.replace(/ /g, "");
+    const frase2 = [...frase1].reverse().join("");
     
+    if (frase1 == frase2) {
+        resp.innerText = `A frase ${frase.toUpperCase()} é Polindromo`;
+    } else {
+        resp.innerText = `A frase ${frase.toUpperCase()} Não é polindromo`;
+    }
+
+    frm.inFrase.value = "";
+    frm.inFrase.focus();
 })
