d579df4306f5ae56de1f30d34c448a657b45f747
Guilherme Monteiro <guilhermemonteiroduarte851@gmail.com>
2026-04-15 16:13:45 -0300

    Update ex8_2.js

---
+++ cap08/script/ex8_2.js
@@ -0,0 +1,36 @@
+const frm = document.querySelector('form');// obtem os elementos da pagina
+const resp1 = document.querySelector('#outResp1');
+const resp2 = document.querySelector('#outResp2');
+const resp3 = document.querySelector('#outResp3');
+
+function classificarVeiculo(ano) {
+    const AnoAtual = new Date().getFullYear();
+    let classif
+    if (ano == AnoAtual) {
+        classif = 'Novo';
+    } else if (ano == AnoAtual - 1 || ano == AnoAtual -2) {
+        classif = 'Seminovo';
+    } else {
+        classif =  'Usado';
+    }
+
+    return classif;
+}
+
+
+
+frm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const modelo = frm.inModelo.value;
+    const AnoFabricado = Number(frm.inAno.value);
+    const preco = Number(frm.inPreço.value);
+
+    const classificaçao = classificarVeiculo(ano);
+    const entrada = calcularEntrada(preço, classificaçao);
+    const parcela = (preco - entrada) / 10; // usa o retorno da função para cálculo.
+
+    resp1.innerText = modelo + '-' + classificaçao;
+    resp2.innerText = `Entrada R$ ${entrada.toFixed(2)}`;
+    resp3.innerText = `+10x de R$ ${parcela.toFixed(2)}`;
+})
