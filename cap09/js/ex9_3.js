b88b7651c057d9aee215991e488fdaa0e194a926
Guilherme Monteiro <guilhermemonteiroduarte851@gmail.com>
2026-07-10 14:08:51 -0300

    feat: add form handling for weight betting with localStorage support

---
+++ cap09/js/ex9_3.js
@@ -0,0 +1,31 @@
+const frm = document.querySelector("form");
+const respLista = document.querySelector("pre");
+
+frm.addEventListener('submit', (e) => {
+    e.preventDefault(); // evita o envio do formulario
+
+    const nome = frm.inNome.value;
+    const peso = Number(frm.inPeso.value);
+
+    //chama a função que verifica se peso ja foi apostado
+    if (verApostaExiste(peso)) {
+        alert("Alguem já apostou este peso, informe outro...");
+        frm.inPeso.focus();
+        return;
+    }
+
+    if (localStorage.getItem("melanciaNome")) {
+        //obtem o conteudo ja salvo
+        const melanciaNome = localStorage.getItem("melanciaNome") + ";" + nome;
+        const melanciaPeso = localStorage.getItem("melaciaPeso") + ";" + peso;
+        localStorage.setItem("melanciaNome", melanciaNome); // salva os dados
+        localStorage.setItem("melanciaPeso", melanciaPeso);
+    } else {
+        localStorage.setItem("melanciaNome", nome);
+        localStorage.setItem("melanciaPeso", peso);
+    }
+
+    mostrarApostas(); // mostra as apostas ja salvas
+    frm.reset(); // limpa o formulario
+    frm.inNome.focus(); // joga o foco (curso) no campo nome
+});
