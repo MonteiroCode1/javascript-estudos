7b56610e53d2f5137f161d7f68a7f9302bcc9162
Guilherme Monteiro <guilhermemonteiroduarte851@gmail.com>
2026-07-23 15:37:47 -0300

    Update ex9_3.js

--- cap09/js/ex9_3.js
+++ cap09/js/ex9_3.js
@@ -1,21 +1,51 @@
 const frm = document.querySelector("form");
 const respLista = document.querySelector("pre");
 
+const verApostaExiste = (peso) => {
+    if (localStorage.getItem("melanciaPeso")) {
+        const pesos = localStorage.getItem("melanciaPeso").split(";");
+        return pesos.includes(peso.toString());
+    }
+    else {
+        return false;
+    }
+};
+
+const mostrarApostas = () => {
+    if (!localStorage.getItem("melanciaNome")) {
+        respLista.innerText = "";
+        return; // a execução acaba aqui... (não executa os comandos baixo)
+    }
+
+    // armazenando os valores salvos no localstore em variaveis usando o
+    // split para criar elementos de vetor a cada ;
+    const nome = localStorage.getItem("melanciaNome").split();
+    const peso = localStorage.getItem("melanciaPeso").split();
+
+    let linhas = "";
+
+    for (let i = 0; i < nome.length; i++) {
+        linhas += nome[i] + " - " + peso[i] + "gr \n";
+    }
+
+    respLista.innerText = linhas;
+};
+
+window.addEventListener('load', mostrarApostas);
+
+frm.btVencedor.addEventListener("click", () => {
+    if (!localStorage.getItem("melanciaNome")) {
+        alert("Não há valores armazenados!");
+        return;
+    }
+});
+
 frm.addEventListener('submit', (e) => {
     e.preventDefault(); // evita o envio do formulario
 
     const nome = frm.inNome.value;
     const peso = Number(frm.inPeso.value);
 
-    const verApostaExiste = (peso) => {
-        if (localStorage.getItem("melanciaPeso")) {
-            const pesos = localStorage.getItem("melanciaPeso").split(";");
-            return pesos.includes(peso.toString());
-        }
-        else {
-            return false;
-        }
-    };
 
     //chama a função que verifica se peso ja foi apostado
     if (verApostaExiste(peso)) {
