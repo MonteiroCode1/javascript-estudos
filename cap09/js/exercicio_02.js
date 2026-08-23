831a139cae59da57aa88144e4c2de6af44d1bc81
Guilherme Monteiro <guilhermemonteiroduarte851@gmail.com>
2026-08-22 22:38:24 -0300

    feat: update form styles and improve service management functions

--- cap09/js/exercicio_02.js
+++ cap09/js/exercicio_02.js
@@ -4,7 +4,7 @@ const resp2 = document.querySelector("#inExecucao");
 
 
 // função de cadastro
-function cadastro(servico) {
+function cadastroService(servico) {
     if (localStorage.getItem("servico")) {
         const addServico = localStorage.getItem("servico") + `;${servico}`;
         localStorage.setItem("servico", addServico);
@@ -12,4 +12,46 @@ function cadastro(servico) {
         localStorage.setItem("servico", servico);
     }
 }
+
+//função de remover o primeiro serviço e exibi-lo na página
+function executarService() {
+    let temp = ``;
+    if (localStorage.getItem("servico")) {
+        const delServico = localStorage.getItem("servico").split(";");
+        const valorDel = delServico.shift();
+        for (let i = 0; i < delServico.length; i++) {
+            if (temp) {
+                temp += `;${delServico[i]}`;
+            } else {
+                temp += delServico[i];
+            }
+        }
+        localStorage.setItem("servico", temp);
+        resp2.innerText = valorDel;
+    }
+}
+
+//função que mostra quantos serviços pendentes tem
+function pendenteService() {
+    if (localStorage.getItem("servico")) {
+        const valorX = localStorage.getItem("servico").split(";").length;
+        resp1.innerText = `Serviços Pendentes: ${valorX}`;
+    } else {
+        resp1.innerText = 'Serviços Pendentes: 0';
+    }
+}
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const servico = frm.inService.value;
+    cadastroService(servico);
+    pendenteService();
+    frm.inService.value = "";
+    frm.inService.focus();
+});
+
+frm.inExecutar.addEventListener("click", () => {
+    executarService();
+    pendenteService();
+});
 
