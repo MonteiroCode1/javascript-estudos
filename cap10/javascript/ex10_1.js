11589e2183eefbe1bfce49de26201bb9fbbd8fe7
Guilherme Monteiro <guilhermemonteiroduarte851@gmail.com>
2026-08-27 16:47:56 -0300

    feat: add task selection functionality to manage displayed tasks

--- cap10/javascript/ex10_1.js
+++ cap10/javascript/ex10_1.js
@@ -15,3 +15,29 @@ frm.addEventListener("submit", (e) => {
     frm.inTarefa.focus();
 });
 
+frm.btSelecionar.addEventListener("click", () => {
+    const tarefas = document.querySelectorAll("h5");
+
+    if (tarefas.length == 0) {
+        alert("Não há tarefas para selecionar");
+        return;
+    }
+
+    let aux = -1; // variavel auxiliar para indicar linha selecionada
+
+    // percorre as tarefas
+    for (let i = 0; i < tarefas.length; i++) {
+        if (tarefas[i].className == "tarefa-selecionada") {
+            tarefas[i].className = "tarefa-normal"; // troca para normal
+            aux = i;
+            break;
+        }
+    }
+    // se a linha que esta selecionada é a ultema, irá voltar para a primeira.
+    if (aux == tarefas.length - 1) {
+        aux = -1;
+    }
+
+    tarefas[aux + 1].className = "tarefa-selecionada"; //muda o estilo da propria linha.
+});
+
