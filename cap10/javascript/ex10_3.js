b3eeb9b5639f2a3407a187b59922723f78e37cba
monteirocode1 <guilhermemonteiroduarte851@gmail.com>
2026-09-10 16:14:17 -0300

    feat: add movie deletion functionality with local storage update

--- cap10/javascript/ex10_3.js
+++ cap10/javascript/ex10_3.js
@@ -41,6 +41,32 @@ frm.addEventListener("submit", (e) => {
     frm.inTitulo.focus(); // posiciona o curso em inTitulo
 })
 
+tbFilmes.addEventListener("click", (e) => {
+    // se a classe do elemento alvo clicado contem excluir
+    if (e.target.classList.contains("exclui")) {
+        // acessa o Pai do Pai do elemento alvo, e obtem o texto do 1 filho
+        const titulo = e.target.parentElement.parentElement.children[0].innerText;
+
+        if (confirm(`Confirma a exclusçao do filme: ${titulo} ?`)) {
+            // remove a linha da tabela correspondente ao simbolo de excluir clicado
+            e.target.parentElement.parentElement.remove();
+
+
+            // exclui os filmes salvos no local storage
+            localStorage.removeItem("filmesTitulos");
+            localStorage.removeItem("filmesGenero");
+
+            // salva novamente (se existir), acessando o conteudo da tabela
+            for (let i = 1; i < tbFilmes.rows.length; i++) {
+                // obtem o conteudo da tabela (coluna 0: titulo, coluna 1: genero)
+                const auxTitulo = tbFilmes.rows[i].cells[0].innerText;
+                const auxGenero = tbFilmes.rows[i].cells[1].innerText;
+                gravarFilme(auxTitulo, auxGenero);
+            }
+        }
+    }
+})
+
 window.addEventListener("load", () => {
     if (localStorage.getItem("filmesTitulos")) {
 
